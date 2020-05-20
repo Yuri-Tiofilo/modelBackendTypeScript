3e5d9d8d3171adf0f97940eb8ef4743f9d69c642
Yuri Silva <yusilva12345@gmail.com>
2020-05-19 23:06:40 -0300

    Initial TDD

---
+++ src/modules/apppointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,8 @@
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', () => {});
+  // it('should note be able to create a two appointments on the same time date', () => {
+  //   expect();
+  // });
+});
